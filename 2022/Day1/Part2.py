3975e3a43721dfc2fc78a486c8434fb8ac70d06f
Mrinal Mukherjee <mrinaldevops@gmail.com>
2023-09-29 23:33:00 +0400

    added part2

--- 2022/Day1/Part2.py
+++ 2022/Day1/Part2.py
@@ -5,11 +5,15 @@ Lines = file1.readlines()
 arr_overall_count = []
 elf_cal_count = 0
 for line in Lines:
-    if (line.strip() == ""):
+    if (line == "\n"):
         # push the calorie count to the final array and reset the elf calorie count
         arr_overall_count.append(elf_cal_count)
         elf_cal_count = 0
     else:
         # increment the calorie count
         elf_cal_count = elf_cal_count + int(line.strip())
-print ("Maximum Value is: " + str(max(arr_overall_count)))
+
+# sort in descending order
+arr_overall_count.sort(reverse=True)
+sum_top_three = arr_overall_count[0] + arr_overall_count[1] + arr_overall_count[2]
+print ("Sum of top 3 values is: " + str(sum_top_three))
