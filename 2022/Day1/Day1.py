65d13050c6c971186daee613b35a2480eb535ee3
Mrinal Mukherjee <mrinaldevops@gmail.com>
2023-09-29 23:07:30 +0400

    Day 1

---
+++ 2022/Day1/Day1.py
@@ -0,0 +1,15 @@
+# Read the input file
+file1 = open('Day1/input.txt', 'r')
+Lines = file1.readlines()
+ 
+arr_overall_count = []
+elf_cal_count = 0
+for line in Lines:
+    if (line.strip() == ""):
+        # push the calorie count to the final array and reset the elf calorie count
+        arr_overall_count.append(elf_cal_count)
+        elf_cal_count = 0
+    else:
+        # increment the calorie count
+        elf_cal_count = elf_cal_count + int(line.strip())
+print ("Maximum Value is: " + str(max(arr_overall_count)))
